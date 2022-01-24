6465853f17eb649b02910ca3b77741ae5daa24f0
Satvik Agrawal <satvik.agrawal2001@gmail.com>
2022-01-24 14:05:30 +0530

    hm

--- license_plate_web/settings.py
+++ license_plate_web/settings.py
@@ -137,7 +137,5 @@ STATICFILES_DIRS = (
 import django_heroku
 django_heroku.settings(locals())
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
